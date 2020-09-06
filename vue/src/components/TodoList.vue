45426a55e252ffba8ffe47a5746272749de0fd45
Matt Wright <mattwright3195@gmail.com>
2020-09-06 14:34:09 -0700

    working

--- vue/src/components/TodoList.vue
+++ vue/src/components/TodoList.vue
@@ -1,24 +1,23 @@
 <template>
-  <div>Testing</div>
+  <ul>
+    <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" />
+  </ul>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { useQuery } from '@vue/apollo-composable'
-import gql from 'graphql-tag'
+import { useTodosQuery } from '@/generated/graphql'
+import { useResult } from '@vue/apollo-composable'
+import TodoItem from '@/components/TodoItem.vue'
 
 export default defineComponent({
+  components: { TodoItem },
   props: {},
   setup() {
-    const { result } = useQuery(gql`
-      query Todos {
-        todos {
-          id
-          text
-        }
-      }
-    `)
-    console.log(result)
+    const { result } = useTodosQuery()
+    const todos = useResult(result, [], (data) => data.todos)
+
+    return { todos }
   },
 })
 </script>
